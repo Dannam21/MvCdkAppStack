213fd0d2a0c729b36d38758a60943ca166c2593f
Dannam21 <danna.mendez@utec.edu.pe>
2024-09-15 22:04:49 -0500

    Primer commit con todos los archivos

--- bin/mv-cdk-app.ts
+++ bin/mv-cdk-app.ts
@@ -2,9 +2,43 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { MvCdkAppStack } from '../lib/mv-cdk-app-stack';
+import { DefaultStackSynthesizer } from 'aws-cdk-lib';
+
+
+const defaultStackSynthesizer = new DefaultStackSynthesizer({
+  fileAssetsBucketName:
+    "cdk-${Qualifier}-assets-${AWS::AccountId}-${AWS::Region}",
+  bucketPrefix: "",
+
+  imageAssetsRepositoryName:
+    "cdk-${Qualifier}-container-assets-${AWS::AccountId}-${AWS::Region}",
+
+  deployRoleArn: "arn:${AWS::Partition}:iam::${AWS::AccountId}:role/LabRole",
+  deployRoleExternalId: "",
+
+  fileAssetPublishingRoleArn:
+    "arn:${AWS::Partition}:iam::${AWS::AccountId}:role/LabRole",
+  fileAssetPublishingExternalId: "",
+
+  imageAssetPublishingRoleArn:
+    "arn:${AWS::Partition}:iam::${AWS::AccountId}:role/LabRole",
+  imageAssetPublishingExternalId: "",
+
+  cloudFormationExecutionRole:
+    "arn:${AWS::Partition}:iam::${AWS::AccountId}:role/LabRole",
+
+  lookupRoleArn: "arn:${AWS::Partition}:iam::${AWS::AccountId}:role/LabRole",
+  lookupRoleExternalId: "",
+
+  bootstrapStackVersionSsmParameter: "/cdk-bootstrap/${Qualifier}/version",
+
+  generateBootstrapVersionRule: true,
+});
+
 
 const app = new cdk.App();
 new MvCdkAppStack(app, 'MvCdkAppStack', {
+  synthesizer: defaultStackSynthesizer
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -19,3 +53,5 @@ new MvCdkAppStack(app, 'MvCdkAppStack', {
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+
+
--- lib/mv-cdk-app-stack.ts
+++ lib/mv-cdk-app-stack.ts
@@ -1,16 +1,75 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps, Duration, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
-export class MvCdkAppStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class MvCdkAppStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'MyVpc', {
+      maxAzs: 3
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MvCdkAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const role = iam.Role.fromRoleArn(this, 'LabRole', 'arn:aws:iam::020554080627:role/LabRole');
+
+    const machineImage = ec2.MachineImage.genericLinux({
+      'us-east-1': 'ami-0aa28dab1f2852040'
+    });
+    const instanceType = ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO);
+
+    const securityGroup = new ec2.SecurityGroup(this, 'MySecurityGroup', {
+      vpc: vpc,
+      description: 'Allow SSH and HTTP access', 
+      allowAllOutbound: true
+    });
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Permitir acceso SSH');
+    securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Permitir acceso HTTP');
+
+    const instance = new ec2.Instance(this, 'EC2Instance', {
+      instanceType: instanceType,
+      machineImage: machineImage,
+      vpc: vpc,
+      role: role,
+      securityGroup: securityGroup,
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+      keyName: 'vockey', 
+      blockDevices: [
+        {
+          deviceName: '/dev/sda1',
+          volume: ec2.BlockDeviceVolume.ebs(20) 
+        }
+      ],
+    });
+
+    const userData = ec2.UserData.custom(`
+      #!/bin/bash
+      cd /var/www/html/
+      git clone https://github.com/utec-cc-2024-2-test/websimple.git
+      git clone https://github.com/utec-cc-2024-2-test/webplantilla.git
+      ls -l
+    `);
+    instance.addUserData(userData.render());
+
+    new CfnOutput(this, 'InstanceId', {
+      value: instance.instanceId,
+      description: 'ID de la instancia EC2'
+    });
+
+    new CfnOutput(this, 'InstancePublicIP', {
+      value: instance.instancePublicIp,
+      description: 'IP pública de la instancia'
+    });
+
+    new CfnOutput(this, 'websimpleURL', {
+      value: `http://${instance.instancePublicIp}/websimple`,
+      description: 'URL de websimple'
+    });
+
+    new CfnOutput(this, 'webplantillaURL', {
+      value: `http://${instance.instancePublicIp}/webplantilla`,
+      description: 'URL de webplantilla'
+    });
+    
   }
 }
